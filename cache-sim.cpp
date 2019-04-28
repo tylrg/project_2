be41f905aa735ef41a596d88971e41964690c0cc
Tyler Gabriel <tygabriel7@gmail.com>
2019-04-27 21:26:23 -0400

    Set-Associative and No Miss
    yurt skeet damn bitch

--- cache-sim.cpp
+++ cache-sim.cpp
@@ -6,7 +6,9 @@
 using namespace std;
 
 string directMap(vector<long> accesses,int cacheSize);
-string setAssociative(vector<long> accesses;int way);
+string setAssociative(vector<long> accesses,int way);
+
+string setAssociativeWriteMiss(vector<long> accesses, vector<int> ls_type_queue, int way);
 
 int main(int argc, char*argv[]){
 	if(argc!=3){
@@ -64,6 +66,9 @@ int main(int argc, char*argv[]){
 	//FILE *output = fopen(output_file,"w");
 	int kb=1024;
 	cout<<directMap(accesses,1*kb)<<directMap(accesses,4*kb)<<directMap(accesses,16*kb)<<directMap(accesses,32*kb)<<endl;
+	cout<<setAssociative(accesses,2)<<setAssociative(accesses,4)<<setAssociative(accesses,8)<<setAssociative(accesses,16)<<endl;
+	cout<<setAssociativeWriteMiss(accesses,ls_type_queue,2)<<setAssociativeWriteMiss(accesses,ls_type_queue,4)<<setAssociativeWriteMiss(accesses,ls_type_queue,8)<<setAssociativeWriteMiss(accesses,ls_type_queue,16)<<endl;
+
 
 	return 0;
 }
@@ -100,12 +105,13 @@ string directMap(vector<long> accesses,int cacheSize){
 	output.append(";");
 	return output;
 }
-
 string setAssociative(vector<long> accesses,int way){
 	string output="";
 	int hit=0;
 	int miss=0;
+
 	int numEntries=(16*1024/32);
+	int numSets=numEntries/way;
 	long cache [numEntries/way][way];
 	int lruValues [numEntries/way][way];
 	for(int c=0;c<numEntries/way;c++){
@@ -118,16 +124,125 @@ string setAssociative(vector<long> accesses,int way){
 
 	for(unsigned int i=0;i<accesses.size();i++){
 		long instruction=accesses[i];
-		int setIndex=instruction%way;
-		//int tag=instruction>>5;
-		//int index= tag%entries;
+		int tag=instruction>>5;
+		int setIndex=tag%numSets;
 
-		if(cache[index]==(32*tag)){
-			hit++;
+		int hFlag=0; //determines hit or miss
+		for(int w=0;w<way;w++){
+			if(cache[setIndex][w]==(32*tag)){
+				hit++;
+				hFlag=1;
+				lruValues[setIndex][w]=0;
+			}
 		}
-		else{
-			cache[index]=tag*32;
+		//if miss
+		//double check this shit
+		if(hFlag==0){
+			int fullFlag=1;
+			for(int m=0;m<way;m++){
+				if(cache[setIndex][m]==0){
+					cache[setIndex][m]=tag*32;
+					lruValues[setIndex][m]=0;
+					fullFlag=0;
+					break;
+				}
+			}
+
+			//if victim selection is needed
+			if(fullFlag==1){
+				int vIndex=0;
+				for(int l=1;l<way;l++){
+					if(lruValues[setIndex][l]>lruValues[setIndex][vIndex]){
+						vIndex=l;
+					}
+				}
+				cache[setIndex][vIndex]=tag*32;
+				lruValues[setIndex][vIndex]=0;
+			}
 			miss++;
+		}
+
+		for(int lr=0;lr<numSets;lr++){
+			for(int lc=0;lc<way;lc++){
+				if(lruValues[lr][lc]!=-1){lruValues[lr][lc]++;}
+			}
+		}
+	}
+
+	int total=hit+miss;
+	string hitS= to_string(hit);
+	string totalS=to_string(total);
+	output.append(hitS);
+	output.append(",");
+	output.append(totalS);
+	output.append(";");
+	return output;
+}
+
+string setAssociativeWriteMiss(vector<long> accesses,vector<int> ls_type_queue,int way){
+	string output="";
+	int hit=0;
+	int miss=0;
+
+	int numEntries=(16*1024/32);
+	int numSets=numEntries/way;
+	long cache [numEntries/way][way];
+	int lruValues [numEntries/way][way];
+	for(int c=0;c<numEntries/way;c++){
+		for(int k=0;k<way;k++){
+			cache[c][k]= 0;
+			lruValues[c][k]= -1;
+		}
+	}
+
+
+	for(unsigned int i=0;i<accesses.size();i++){
+		long instruction=accesses[i];
+		int tag=instruction>>5;
+		int setIndex=tag%numSets;
+
+		int hFlag=0; //determines hit or miss
+		for(int w=0;w<way;w++){
+			if(cache[setIndex][w]==(32*tag)){
+				hit++;
+				hFlag=1;
+				lruValues[setIndex][w]=0;
+			}
+		}
+		//if miss
+		//double check this shit
+		if((hFlag==0)&&(ls_type_queue[i]!=0)){
+			int fullFlag=1;
+			for(int m=0;m<way;m++){
+				if(cache[setIndex][m]==0){
+					cache[setIndex][m]=tag*32;
+					lruValues[setIndex][m]=0;
+					fullFlag=0;
+					break;
+				}
+			}
+
+			//if victim selection is needed
+			if(fullFlag==1){
+				int vIndex=0;
+				for(int l=1;l<way;l++){
+					if(lruValues[setIndex][l]>lruValues[setIndex][vIndex]){
+						vIndex=l;
+					}
+				}
+				cache[setIndex][vIndex]=tag*32;
+				lruValues[setIndex][vIndex]=0;
+			}
+			miss++;
+		}
+		else if((hFlag==0)&&(ls_type_queue[i]==0)){
+			miss++;
+		}
+
+		for(int lr=0;lr<numSets;lr++){
+			for(int lc=0;lc<way;lc++){
+				if(lruValues[lr][lc]!=-1){lruValues[lr][lc]++;}
+			}
 		}
 	}
 
